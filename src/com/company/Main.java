67e8299b4327cd0a802ad93e00ce4577fab88be9
Elizabeth8353 <evaldivieso6@gmail.com>
2021-11-08 17:40:02 -0500

    feature Sun added with a bug that needs be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,13 @@ public class Main {
         andromeda.moon=4;
                 andromeda.planet=2;
                         andromeda.Sun=1;
+featureSun mikesun=new featureSun();
+mikesun.color="blue";
+mikesun.heat="680024520";
+mikesun.temp="5celsius";
+mikesun.radius="0.009";
+mikesun.maritalStatus="single";
+
 
 
     }
