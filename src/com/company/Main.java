b6d2f5d930dcd1b30ea99e85bae73f9fabd24536
Elizabeth8353 <evaldivieso6@gmail.com>
2021-11-08 08:17:24 -0500

    Solar System Basic Structure is Ready 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem andromeda=new SolarSystem();
+        andromeda.moon=4;
+                andromeda.planet=2;
+                        andromeda.Sun=1;
+
+
+    }
+}
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class SolarSystem {
+
+    int Sun;
+    int planet;
+    int moon;
+
+
+}
